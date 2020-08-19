1d127701d7821bd5255b682457a9adc56ef43d65
Lipkon <47846542+Lipkon@users.noreply.github.com>
2020-08-19 19:28:16 +0300

    f2

---
+++ precache-manifest.e07d8f68e87a0b8d1bfeb924f504aeed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a936f0070fad05790b2dff1d1e67c0db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a621ce0ec13349cd817",
+    "url": "/static/css/3.0c5712b2.chunk.css"
+  },
+  {
+    "revision": "7e279aa4bf1802792ab2",
+    "url": "/static/css/main.a23761d6.chunk.css"
+  },
+  {
+    "revision": "cdc8d0f09613078cc0ab",
+    "url": "/static/js/2.762ea9e2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.762ea9e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a621ce0ec13349cd817",
+    "url": "/static/js/3.86623660.chunk.js"
+  },
+  {
+    "revision": "7e279aa4bf1802792ab2",
+    "url": "/static/js/main.fc8b01ae.chunk.js"
+  },
+  {
+    "revision": "c719105ad99d1ee86380",
+    "url": "/static/js/runtime-main.95d261d5.js"
+  },
+  {
+    "revision": "5422ce1ea6e634773613df84452620d4",
+    "url": "/static/media/Infinity.5422ce1e.svg"
+  }
+]);
